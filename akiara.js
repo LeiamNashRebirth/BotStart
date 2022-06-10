b8a4d42c45dde1e8d36decac6d9329217a992258
Leiam Nash <94122613+LeiamNashRebirth@users.noreply.github.com>
2022-06-10 09:34:16 +0800

    Create akiara.js

---
+++ akiara.js
@@ -0,0 +1,137 @@
+{
+    "language": "en",
+    "keyActive": "",
+    "DeveloperMode": false,
+    "autoCreateDB": true,
+    "allowInbox": true,
+    "autoClean": true,
+    "commandDisabled": [],
+    "eventDisabled": [],
+    "EMAIL": "",
+    "PASSWORD": "",
+    "OTPKEY": "",
+    "BOTNAME": "LeiamNash",
+    "PREFIX": "+",
+    "ADMINBOT": [
+      
+    ],
+    "DATABASE": {
+        "sqlite": {
+            "storage": "data.sqlite"
+        }
+    },
+    "APPSTATEPATH": "appstate.json",
+    "FCAOption": {
+        "forceLogin": true,
+        "listenEvents": true,
+        "pauseLog": true,
+        "logLevel": "error",
+        "selfListen": true,
+        "userAgent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.72 Safari/537.36"
+    },
+    "version": "1.2.9",
+    "checktt": {
+        "maxColumn": 10
+    },
+    "daily": {
+        "cooldownTime": 150000,
+        "rewardCoin": 50
+    },
+    "help": {
+        "autoUnsend": true,
+        "delayUnsend": 10000000
+    },
+    "math": {
+        "WOLFRAM": "T8J8YV-H265UQ762K"
+    },
+    "rank": {
+        "APIKEY": "170440784240186|bc82258eaaf93ee5b9f577a8d401bfc9"
+    },
+    "rankup": {
+        "autoUnsend": false,
+        "unsendMessageAfter": 50000000
+    },
+    "rname": {
+        "APIKEY": "mi451266190"
+    },
+    "sauce": {
+        "SAUCENAO_API": "AIzaSyB6pTkV2PM7yLVayhnjDSIM0cE_MbEtuvo"
+    },
+    "sing": {
+        "YOUTUBE_API": "AIzaSyCs9bhREBvvLfmAmF0QqWVR3zziZpqwzWY",
+        "SOUNDCLOUD_API": "M4TSyS6eV0AcMynXkA3qQASGcOFQTWub"
+    },
+    "video": {
+        "YOUTUBE_API": "AIzaSyCs9bhREBvvLfmAmF0QqWVR3zziZpqwzWY"
+    },
+    "weather": {
+        "OPEN_WEATHER": "081c82065cfee62cb7988ddf90914bdd"
+    },
+    "work": {
+        "cooldownTime": 700000
+    },
+    "adminUpdate": {
+        "autoUnsend": false,
+        "sendNoti": false,
+        "timeToUnsend": 10
+    },
+    "sim": {
+        "APIKEY": "MEWU2VudGhhbmg7c2VuODU3NDc0QGdtYWlsLmN"
+    },
+    "surprise": {
+        "log": true,
+        "out": "",
+        "kickAll": "",
+        "delThread": ""
+    },
+    "SOUNDCLOUD_API": {
+        "APIKEY": "M4TSyS6eV0AcMynXkA3qQASGcOFQTWub"
+    },
+    "ghep": {
+        "APIKEY": "531532681600063|PZKvEhQ2eQ_XGkh5GlhMv6HBS60"
+    },
+    "info": {
+        "APIKEY": "531532681600063|PZKvEhQ2eQ_XGkh5GlhMv6HBS60"
+    },
+    "log": {
+        "enable": true
+    },
+    "covid": {
+        "APIKEY": "MEWQmluZHo7YmludGFwaGFjaDFAZ21haWwuY29"
+    },
+    "hitbutt": {
+        "APIKEY": "MEWQmluZHo7YmludGFwaGFjaDFAZ21haWwuY29"
+    },
+    "img": {
+        "APIKEY": "MEWQmluZHo7YmludGFwaGFjaDFAZ21haWwuY29"
+    },
+    "linkword": {
+        "APIKEY": "MEWQmluZHo7YmludGFwaGFjaDFAZ21haWwuY29"
+    },
+    "sortword": {
+        "APIKEY": "MEWQmluZHo7YmludGFwaGFjaDFAZ21haWwuY29"
+    },
+    "teach": {
+        "APIKEY": "MEWQmluZHo7YmludGFwaGFjaDFAZ21haWwuY29"
+    },
+    "slap": {
+        "APIKEY": "MEWQmluZHo7YmludGFwaGFjaDFAZ21haWwuY29"
+    },
+    "menu": {
+        "autoUnsend": false,
+        "unsendMessageAfter": 500000,
+        "delayUnsend": 6000000
+    },
+    "caesar": {
+        "caeserPassword": "266303"
+    },
+    "cave": {
+        "cooldownTime": 1000000
+    },
+    "diemdanh": {
+        "cooldownTime": 3000000
+    },
+    "tiktok": {
+        "APIKEY": ""
+    }
+}
